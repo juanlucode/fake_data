55014f3f287c2b94ef4ab27f7dd25e026d5af9bb
juanluisga <juanluis.garciarodriguez@gmail.com>
2017-08-03 08:56:16 +0200

    delete Countries.kt

--- src/io/github/juanlucode/fakedata/model/Countries.kt
+++
@@ -1,8 +0,0 @@
-package io.github.juanlucode.fakedata.model
-
-/*
- For code uses ISO code with 3 chars ( https://countrycode.org/ )
-*/
-
-data class Country(val code : String, val name : String) {
-}
